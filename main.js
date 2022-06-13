54a548219e67668d10e385b2a0e5086900f02ffd
Daniel Chang <daniel.changk@gmail.com>
2022-06-12 21:14:33 -0500

    coded most of demo frontend

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let products = null;
+let games = null;
+let selectedGameId = null;
+let selectedGameData = null;
 let categories = null;
 let title = null;
+let sport = "basketball";
 
-// Called once the page has loaded
-document.addEventListener("DOMContentLoaded", function (event) {
-  loadProducts();
-  loadCategories();
-});
-
+const defaultUrl =
+  "https://docs.google.com/spreadsheets/d/1lePLaeywOD97irRoD7qWcE3GHFMYimTPX1g77Vj-Yz8/edit?usp=sharing";
+const statifyUrl = "http://127.0.0.1:5000";
 const DUMMY_PRODUCTS = [
   {
     name: "Sheety",
@@ -19,61 +19,117 @@ const DUMMY_PRODUCTS = [
     id: 2,
   },
 ];
-
-const DUMMY_CATEGORIES = [
+const CATEGORIES = [
   {
     name: "Box Score",
+    recordType: "box_score",
     id: 1,
   },
   {
     name: "Play-by-Play",
+    recordType: "pbp",
     id: 2,
   },
   {
     name: "Team Stats",
+    recordType: "team_stats",
     id: 3,
   },
 ];
 
-// Replace this with your Sheety URL
-// Make sure NOT to include the sheet name in the URL (just the project name!)
-// var projectUrl =
-//   "https://api.sheety.co/e1cfbe6d472149b2a7874ea91d598f72/statify";
+const CATEGORY_HEADINGS = {
+  games: [
+    "Game #",
+    "Date Played",
+    "Team 1",
+    "Team 2",
+    "Team 1 Score",
+    "Team 2 Score",
+  ],
+  box_score: [
+    "PLAYER",
+    "MIN",
+    "FG",
+    "3PT",
+    "FT",
+    "OREB",
+    "DREB",
+    "REB",
+    "AST",
+    "STL",
+    "BLK",
+    "TO",
+    "PF",
+    "PTS",
+  ],
+  pbp: ["QUARTER", "TIME", "TEAM", "PLAY", "SCORE"],
+  team_stats: [
+    "PTS",
+    "FG",
+    "Field Goal %",
+    "3PT",
+    "Three Point %",
+    "FT",
+    "Free Throw %",
+    "OREB",
+    "DREB",
+    "REB",
+    "AST",
+    "STL",
+    "BLK",
+    "TO",
+    "PF",
+  ],
+};
 
-function loadProducts() {
-  this.products = DUMMY_PRODUCTS.sort((a, b) => {
-    return a.votes < b.votes;
-  });
-  showAllProducts();
+const CATEGORY_ALIGNMENT = {
+  games: "center",
+  box_score: "right",
+  pbp: "left",
+};
 
-  //   fetch(projectUrl + "/products")
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       this.products = json.products.sort((a, b) => {
-  //         return a.votes < b.votes;
-  //       });
-  //       showAllProducts();
-  //     });
+// Called once the page has loaded
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadGames();
+  loadCategories();
+});
+
+function loadGames() {
+  selectedGameId = null;
+  loadCategories();
+  fetch(statifyUrl + "/api/basketball/game/all")
+    .then((response) => response.json())
+    .then((data) => {
+      const { records } = data["games"];
+      showAllGames(records);
+    })
+    .catch((err) => {
+      showAllGames();
+      return;
+    });
 }
 
 function loadCategories() {
-  this.categories = DUMMY_PRODUCTS.length > 0 ? DUMMY_CATEGORIES : [];
+  this.categories = selectedGameId ? CATEGORIES : [];
   drawCategories();
-  //   fetch(projectUrl + "/categories")
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       this.categories = json.categories;
-  //       drawCategories();
-  //     });
 }
 
-function drawProducts(products) {
+function drawRecords(recordsList, record_type) {
+  headings = CATEGORY_HEADINGS[record_type];
+  recordsList = recordsList.map((recordsListObj) => {
+    recordsListObj.headings = headings;
+    return recordsListObj;
+  });
   const template = Handlebars.compile(
-    document.getElementById("products-template").innerHTML
+    document.getElementById("records-template").innerHTML
   );
-  document.getElementById("products-container").innerHTML = template({
+  const foundRecords = !recordsList[0].records;
+  document.getElementById("records-container").innerHTML = template({
     title: this.title,
-    products: products,
+    headings: headings,
+    recordsList: recordsList,
+    foundRecords: foundRecords,
+    errorMessage: "No records found",
   });
 }
 
@@ -81,39 +137,65 @@ function drawCategories() {
   const template = Handlebars.compile(
     document.getElementById("menu-template").innerHTML
   );
-  console.log("draw ", this.products);
   document.getElementById("menu-container").innerHTML = template(
     this.categories
   );
 }
 
-function showAllProducts() {
-  this.title = "All Games";
-  drawProducts(this.products);
+function showAllGames(records) {
+  records_list = new Array();
+  records_list_obj = {
+    listLabel: "All Games",
+    records: records,
+    isGames: true,
+    align: CATEGORY_ALIGNMENT["games"],
+  };
+  records_list.push(records_list_obj);
+  drawRecords(records_list, "games");
 }
 
-function showCategory(category) {
+function showCategory(category, recordType) {
   this.title = category;
-  const filteredProducts = this.products.filter((product) => {
-    return product.category == category;
-  });
-  drawProducts(filteredProducts);
+  player_stat_records = [];
+  for (const [key, values] of Object.entries(selectGameData[recordType])) {
+    if (key !== "headings") {
+      records_list_obj = {
+        listLabel: selectGameData["teams"].includes(key) ? key : null,
+        records: values,
+        align: CATEGORY_ALIGNMENT[recordType],
+      };
+      player_stat_records.push(records_list_obj);
+    }
+  }
+  drawRecords(player_stat_records, recordType);
 }
 
-function upvoteProduct(id) {
-  const product = this.products.find((product) => {
-    return product.id == id;
-  });
-  product.votes = product.votes + 1;
-  product.hasVoted = true;
-
-  //   let headers = new Headers();
-  //   headers.set("content-type", "application/json");
-  //   fetch(projectUrl + "/products/" + id, {
-  //     method: "PUT",
-  //     body: JSON.stringify({ product: product }),
-  //     headers: headers,
-  //   });
-
-  showAllProducts();
+function selectGame(gameId) {
+  gameId = +gameId;
+  getGame(gameId);
 }
+
+function getGame(gameId) {
+  if (gameId === selectedGameId) return;
+
+  fetch(statifyUrl + "/api/basketball/game/" + gameId)
+    .then((response) => response.json())
+    .then((data) => {
+      selectGameData = data;
+      selectedGameId = gameId;
+      loadCategories();
+      showCategory("Box Score", "box_score");
+    })
+    .catch((err) => {
+      // location.reload();
+      return;
+    });
+}
+
+Handlebars.registerHelper("getId", function (values) {
+  return values[0];
+});
+
+Handlebars.registerHelper("getNonIdValues", function (values) {
+  return values.slice(1);
+});
